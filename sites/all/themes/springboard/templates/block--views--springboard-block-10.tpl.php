a586aa76bc1c206bd9069e9de3e52cb1a74e9c12
Sasanka Jandhyala <sasanka.jandhyala@icf.com>
2017-08-28 15:11:48 -0400

    SJ-updated-visionboard-css

--- sites/all/themes/springboard/templates/block--views--springboard-block-10.tpl.php
+++ sites/all/themes/springboard/templates/block--views--springboard-block-10.tpl.php
@@ -1,7 +1,7 @@
 <?php ?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
 <div class="content"<?php print $content_attributes; ?>>
-  <div class="visionboard-icon"></div>
+  <a class="" href="/springboard/get-support/vision-board"><div class="visionboard-icon"></div></a>
   <?php print render($content); ?>
 </div>
 </div>
