2b163c7aa9b859bc7c5b230b60c5350e1dffc289
Sasanka Jandhyala <sasanka.jandhyala@icf.com>
2017-09-07 11:06:07 -0400

    SJ-updated_actiondeck_name_pdf,txt,jpg-updated_css_visionboard

--- sites/all/themes/springboard/templates/block--boxes--video-stories-featured.tpl.php
+++ sites/all/themes/springboard/templates/block--boxes--video-stories-featured.tpl.php
@@ -2,7 +2,7 @@
   <div class="featured-videos-title">
     <h2> <?php print render($title); ?> </h2>
   </div>
-  <div class="content"<?php print $content_attributes; ?>>
+  <div class="content featured-video"<?php print $content_attributes; ?>>
     <?php print render($content); ?>
   </div>
 </div>
--- sites/all/themes/springboard/templates/block--views--springboard-block-10.tpl.php
+++ sites/all/themes/springboard/templates/block--views--springboard-block-10.tpl.php
@@ -1,6 +1,6 @@
 <?php ?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-<div class="content"<?php print $content_attributes; ?>>
+<div class="content getsupport-visionboard"<?php print $content_attributes; ?>>
   <a class="" href="/springboard/get-support/vision-board"><div class="visionboard-icon"></div></a>
   <?php print render($content); ?>
 </div>
