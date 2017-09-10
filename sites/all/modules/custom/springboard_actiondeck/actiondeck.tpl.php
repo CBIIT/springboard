5803fcc08ac8e890a553f20831ca06f7235f4620
Sasanka Jandhyala <sasanka.jandhyala@icf.com>
2017-09-10 16:02:49 -0400

    SJ-Updated_libraries_for_title_not_showing-Updated_Action_deck_print

--- sites/all/modules/custom/springboard_actiondeck/actiondeck.tpl.php
+++ sites/all/modules/custom/springboard_actiondeck/actiondeck.tpl.php
@@ -45,7 +45,10 @@
             <li><a href="https://twitter.com/home?status=<?php print urlencode('Check out my customized Action Deck from Springboard Beyond Cancer at ' . url('/springboard/actiondeck/' . $eid, array('absolute' => TRUE))); ?>"><div class="twitter-icon"></div>Twitter</a></li>
           </ul>
         </div> <!-- end share -->
-        <a href="/springboard/print-action-deck-pdf/<?php print $eid; ?>" target="_top" class="btn-blue"><div class="print-icon"></div>PRINT</a>
+
+        <!-- Print Action Deck -->
+        <a href="" onclick="var w = window.open('/springboard/print-action-deck/<?php print $eid; ?>','_blank'); w.print();" target="_top" class="btn-blue"><div class="print-icon"></div>PRINT</a>
+
         <a href="mailto:?subject=Springboard%20Beyond%20Cancer%20Customized%20Action%20Deck&body=Check%20out%20your%20customized%20Action%20Deck%20from%20Springboard%20Beyond%20Cancer%20at%20<?php print url('springboard/actiondeck/' . $eid, array('absolute' => TRUE)); ?>.%20It%20has%20helpful%20actions%20and%20resources%20for%20dealing%20with%20the%20challenges%20and%20experiences%20of%20cancer."
              target="_top" class="btn-blue"><div class="email-icon"></div>EMAIL</a>
       </div> <!-- end deck-share -->
@@ -66,7 +69,8 @@
         <?php foreach($symptom_cards as $card) : ?>
           	<a class="btn symptoms" role="tree" data-toggle="collapse" data-parent="#accordion" href="#<?php print preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $card['title'])); ?>-card" aria-expanded="false" aria-controls="collapse"><div class="border-to-arrow"></div><div class="btn-title"><?php print $card['title']; ?></div></a>
         	<div id="<?php print preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $card['title'])); ?>-card" role="treeitem" class="collapse card-box-container">
-            	<?php print render(node_view(node_load($card['nid'])))?>
+                <?php $card_load = node_view(node_load($card['nid'])); ?>
+            	<?php print render($card_load);?>
           	</div>
         <?php endforeach; ?>
       <?php endif; ?>
@@ -77,7 +81,8 @@
         <?php foreach($stress_mood_cards as $card) : ?>
           	<a class="btn stressmood" role="tree" data-toggle="collapse" data-parent="#accordion" href="#<?php print preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $card['title'])); ?>-card" aria-expanded="false" aria-controls="collapse"><div class="border-to-arrow"></div><div class="btn-title"><?php print $card['title']; ?></div></a>
         	<div id="<?php print preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $card['title'])); ?>-card" role="treeitem" class="collapse card-box-container">
-            	<?php print render(node_view(node_load($card['nid'])))?>
+              <?php $card_load = node_view(node_load($card['nid'])); ?>
+              <?php print render($card_load);?>
           	</div>
         <?php endforeach; ?>
       <?php endif; ?>
@@ -88,7 +93,8 @@
         <?php foreach($wellness_cards as $card) : ?>
           	<a class="btn wellness" role="tree" data-toggle="collapse" data-parent="#accordion" href="#<?php print preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $card['title'])); ?>-card" aria-expanded="false" aria-controls="collapse"><div class="border-to-arrow"></div><div class="btn-title"><?php print $card['title']; ?></div></a>
         	<div id="<?php print preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $card['title'])); ?>-card" role="treeitem" class="collapse card-box-container">
-            	<?php print render(node_view(node_load($card['nid'])))?>
+              <?php $card_load = node_view(node_load($card['nid'])); ?>
+              <?php print render($card_load);?>
           	</div>
         <?php endforeach; ?>
       <?php endif; ?>
@@ -98,7 +104,8 @@
         <?php foreach($get_support_cards as $card) : ?>
           	<a class="btn getsupport" role="tree" data-toggle="collapse" data-parent="#accordion" href="#<?php print preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $card['title'])); ?>-card" aria-expanded="false" aria-controls="collapse"><div class="border-to-arrow"></div><div class="btn-title"><?php print $card['title']; ?></div></a>
         	<div id="<?php print preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $card['title'])); ?>-card" role="treeitem" class="collapse card-box-container">
-            	<?php print render(node_view(node_load($card['nid'])))?>
+              <?php $card_load = node_view(node_load($card['nid'])); ?>
+              <?php print render($card_load);?>
           	</div>
         <?php endforeach; ?>
       <?php endif; ?>
